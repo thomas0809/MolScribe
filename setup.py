aafe6556380d90267813e2759a686534416a2a2f
uranium11010 <zed.zhening.li@hotmail.com>
2023-04-06 00:07:13 -0400

    Fix dependency versions in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from pathlib import Path
 
 setup(name='MolScribe',
-      version='1.1.0',
+      version='1.1.1',
       description='MolScribe',
       author='Yujie Qian',
       author_email='yujieq@csail.mit.edu',
@@ -14,12 +14,12 @@ setup(name='MolScribe',
       setup_requires=['numpy'],
       install_requires=[
         "numpy",
-        "torch>=1.11.0",
+        "torch>=1.11.0,<2.0",
         "pandas",
         "opencv-python>=4.5.5.64",
         "SmilesPE==0.0.3",
         "OpenNMT-py==2.2.0",
         "rdkit-pypi>=2021.03.2",
-        "albumentations @ git+https://github.com/albumentations-team/albumentations@37e714fd2e326f6f88778e425f98c2de8c8d5372",
-        "timm @ git+https://github.com/rwightman/pytorch-image-models.git@54a6cca27a9a3e092a07457f5d56709da56e3cf5"
+        "albumentations==1.1.0",
+        "timm==0.4.12"
       ])
